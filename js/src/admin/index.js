aacaba3aef3db6250d9068becffae5f4dcf8767b
Nic <varguti@gmail.com>
2021-08-01 17:35:57 +0200

    add logic to handle multiple between-post ids

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,8 +1,8 @@
 import app from "flarum/app";
 import AdsPage from "./components/AdsPage";
 
-app.initializers.add('malago/ads', () => {
+app.initializers.add('insegreto/ads', () => {
     app.extensionData
-        .for("malago-ads")
+        .for("insegreto-ads")
         .registerPage(AdsPage);
 });
--- js/src/forum/addAdBetweenPosts.js
+++ js/src/forum/addAdBetweenPosts.js
@@ -7,19 +7,21 @@ export default function() {
         const advertisement = app.forum.attribute('flagrow.ads.between-posts');
 
         if (advertisement && component.children.length) {
+            const advertisements = advertisement.split(',').map(Function.prototype.call, String.prototype.trim);
             const start = Number(app.forum.attribute('flagrow.ads.start-from-post'));
             const between = Number(app.forum.attribute('flagrow.ads.between-n-posts'));
             // We need to copy all comments first, otherwise -there is no way to detect and jump the last comment
             const commentPosts = component.children.filter(post => post.attrs['data-type'] === 'comment');
-            
+
             // Insert an inside every n comment
             commentPosts.forEach((post, i) => {
                 const postNum = post.attrs['data-number'];
-                if (postNum === start || ((postNum - start) % between) === 0) {
+                const ad = advertisements.shift();
+                if (postNum === start || ((postNum - start) % between) === 0 && ad) {
                     post.children.push(
                         m('div.Flagrow-Ads-fake-poststream-item',
                             m('article.Post.EventPost',
-                                m('div.Flagrow-Ads-between-posts.EventPost-info', m.trust(advertisement))
+                                m('div.Flagrow-Ads-between-posts.EventPost-info', m.trust(ad))
                             )
                         )
                     );
